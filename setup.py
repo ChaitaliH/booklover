268a48c1c9c2e99e13d379f28bdb86e0e7b6820d
ChaitaliH <chaitali.harge@gmail.com>
2023-07-31 18:06:18 -0400

    Added BookLover package files

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='booklover',
+setup(name='book_package',
       version='0.1',
       description='A tool to manage your books!',
       url='https://github.com/ChaitaliH/booklover',
       author='Chaital Harge',
       author_email='atypicalme11@gmail.com',
       license='MIT',
-      packages=['booklover'])
+      packages=['book_package'])
